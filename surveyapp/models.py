5fed784b108d21fb6a41b168f401a5684a2df770
ridgetinez <ridgetinez@gmail.com>
2017-09-05 01:08:52 +1000

    Finished Question Class implementation

--- surveyapp/models.py
+++ surveyapp/models.py
@@ -2,19 +2,25 @@
 class Question(object):
     """ Question object to populate question pools and surveys
     
-    Keywords:
-    questionText | String holds the question displayed to user
-    answerList   | List of strings representing multiple choice answers
-    qid          | Unique identifier for instance
+    Args:
+    question_text -- String of the question displayed to user
+    answer_list   -- Tuple of strings representing multiple choice answers
     """
-    
-    def __init__(self, questionText, answerList, qid):
-        self.questionText = questionText
-        self.answerList = answerList
-        self.qid = qid        
+        
+    def __init__(self, question_text, answer_list):
+        self.question_text = question_text
+        self.answer_list = answer_list
+        self.uqid = id(self)
     
     def __str__(self):
-        pass
+        return '{0}\n{1}'.format(self.question_text, self.answer_list)        
     
-    def add
+    def addAnswerOption(self, answer):
+        self.answer_list.append(answer)
         
+        
+firstQ = Question("How fucked are you for 1531?", ["Yes", "Yes in green", "si est verte"])
+print(firstQ)
+firstQ.addAnswerOption("<3")
+print(firstQ)
+print(getattr(firstQ, 'uqid'))
