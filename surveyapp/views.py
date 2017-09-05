5c036053b9c2044d6c5708963dc4c46e84df3a76
Samuel Smyth <samuelsmyth215@gmail.com>
2017-09-05 13:11:58 +1000

    Add functionality for admin dashboard

--- surveyapp/views.py
+++ surveyapp/views.py
@@ -1,7 +1,14 @@
 from surveyapp import app
 from flask import render_template
 
-#placeholder
 @app.route('/')
 def index():
     return render_template('landing_page.html');
+
+#need to add auth
+@app.route('/dashboard/<sub_page>')
+def admin_dashboard(sub_page):
+    if sub_page == 'surveys':
+        return render_template('admin_dashboard_surveys.html');
+    if sub_page == 'questions':
+        return render_template('admin_dashboard_questions.html');
