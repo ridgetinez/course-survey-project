c00731b4c69609ffaa3a424ff00fe9f9dd7966c9
boboeee <z5080307@cse.unsw.edu.au>
2017-09-05 19:00:05 +1000

    I added a testing html, Just pushing, finished login

--- surveyapp/views.py
+++ surveyapp/views.py
@@ -1,7 +1,23 @@
 from surveyapp import app
 from flask import render_template
+from flask import Flask, flash, redirect, render_template, request, session, abort
 
-#placeholder
 @app.route('/')
-def index():
-    return render_template('base_page.html');
+def home():
+    if not session.get('logged_in'):
+        return render_template('login_page.html')
+    else:
+        return render_template('admin_page.html')
+
+@app.route('/login', methods=['POST'])
+def login():
+	global user_list = UserAuthenticator
+	username = request.form['username']
+	password = request.form['password']
+	if user_list.authenticate(username, password):
+		session['logged_in'] = True
+	else:
+		flash('wrong password!')
+	return home()
+
+
