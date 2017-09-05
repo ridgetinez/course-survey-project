8b0ff5947b1771138ad2ddbd6a3cd700e9e3d191
ridgetinez <ridgetinez@gmail.com>
2017-09-05 10:24:12 +1000

    Implemented QuestionPool as Dict with get/set functionality

--- surveyapp/models.py
+++ surveyapp/models.py
@@ -7,20 +7,78 @@ class Question(object):
     answer_list   -- Tuple of strings representing multiple choice answers
     """
         
+    uqid = 0
+        
     def __init__(self, question_text, answer_list):
-        self.question_text = question_text
-        self.answer_list = answer_list
-        self.uqid = id(self)
+        self.__question_text = question_text
+        self.__answer_list = answer_list
+        self.__qid = Question.uqid
+        Question.uqid += 1
     
     def __str__(self):
-        return '{0}\n{1}'.format(self.question_text, self.answer_list)        
+        return '{0}\n{1}'.format(self.__question_text, self.__answer_list) 
+
+    @property
+    def qid(self):
+        return self.__qid
+           
     
-    def addAnswerOption(self, answer):
-        self.answer_list.append(answer)
+    def add_answer_option(self, answer):
+        self.__answer_list.append(answer)
         
         
 firstQ = Question("How fucked are you for 1531?", ["Yes", "Yes in green", "si est verte"])
 print(firstQ)
-firstQ.addAnswerOption("<3")
+firstQ.add_answer_option("<3")
 print(firstQ)
-print(getattr(firstQ, 'uqid'))
+print(firstQ.qid)
+secondQ = Question("Did you really sleep when you have 1531?", ["Yes", "No", "No in yes."])
+print(secondQ.qid)
+
+
+
+
+class QuestionPool(object):
+    """ Retrieves the collection of created question
+    
+    Args:
+    question_list -- list of questions created
+    """
+    
+    def __init__(self, question_list):
+        q_dict = {}
+        for q in question_list:
+            q_dict[q.qid] = q
+        self.__question_dict = q_dict
+        
+    # LEGACY ITER from when QPOOL was a list. TODO: Change for dicts
+    def __iter__(self):
+        return self.__question_dict
+    
+    def __next__(self):
+        if self.i >= len(self.question_list):
+            raise StopIteration
+        next_question = self.question_list[self.i]
+        self.i += 1
+        return next_question
+        
+        
+    def add_question(self, q):
+        self.__question_dict[q.qid] = q
+        
+    def get_question(self, qid):
+        return self.__question_dict[qid]
+        
+        
+    
+qp = QuestionPool([firstQ, secondQ])
+for q in qp:
+    print(q) 
+        
+        
+        
+        
+        
+        
+        
+        
