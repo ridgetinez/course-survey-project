bdf38f5d555035cbd5e7a31476069515c822a40f
boboeee <z5080307@cse.unsw.edu.au>
2017-09-05 20:47:01 +1000

    testing

--- surveyapp/views.py
+++ surveyapp/views.py
@@ -25,13 +25,11 @@ def login():
 		flash('wrong password!')
 	return home()
 
-def index():
-    return render_template('landing_page.html');
-
 #need to add auth
 @app.route('/dashboard/<sub_page>')
 def admin_dashboard(sub_page):
 	if sub_page == 'surveys':
 		return render_template('admin_dashboard_surveys.html');
 	if sub_page == 'questions':
+		return render_template('admin_dashboard_questions.html');
 		return home()
